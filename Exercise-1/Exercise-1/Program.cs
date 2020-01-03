5d898c3fe8c15f047a23ab31d6487b97d65ead78
Shah Newaz <shahnewaz385@gmail.com>
2020-01-03 21:51:42 +0600

    commite 2 Exercise-1.AssemblyInfo.cs

---
+++ Exercise-1/Exercise-1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Exercise_1
+{
+    
+    class Program
+    {      
+        static void Main(string[]args)
+        {
+            Console.WriteLine("Enter a number between 1 to 10");
+           var m = Console.ReadLine();
+           var k = Convert.ToInt32(m);
+            if(k<=10 && k>=0)
+            {
+                Console.WriteLine("Congrats!!! you enter a valid number");
+            }
+
+            else
+            {
+                Console.WriteLine("You Enter  a Invalid number");
+
+            }
+
+        }
+    }
+}
---
+++ Exercise-2/Program.cs
@@ -0,0 +1,46 @@
+﻿
+
+using System;
+using System.IO;
+using System.Text;
+
+namespace IncludeHelp
+{
+    class Test
+    {
+        // Main Method 
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            int min;
+            int max;
+
+            //input numbers
+            Console.Write("Enter first number : ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter second number: ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            //finding max number using if-else
+            if (a > b)
+                max = a;
+            else
+                max = b;
+
+
+            Console.WriteLine("Maximum number = {0}", max);
+
+            //finding max number using ternary operator
+            //max = (a > b) ? a : b;
+
+
+
+          
+            
+
+            //hit ENTER to exit the program
+            Console.ReadLine();
+        }
+    }
+}
