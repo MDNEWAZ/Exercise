49af7e1fcb22166e9a472739979f2cd59db877d7
Shah Newaz <shahnewaz385@gmail.com>
2020-01-06 12:21:27 +0600

    Exercise-3...

---
+++ Exercise-3.1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Exercise_3._1
+{
+   
+    
+        public enum ImageOrientation
+        {
+            Landscape,
+            Portrait
+        }
+        class Program
+        {
+
+           
+
+            static void Main(string[] args)
+            {
+                Console.Write("Enter The width of the image :");
+                var width = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter the height of the image:");
+                var height = Convert.ToInt32(Console.ReadLine());
+                var orientation = width > height ? ImageOrientation.Landscape : ImageOrientation.Portrait;
+                Console.WriteLine("Image orientation is " + orientation);
+            }
+
+
+
+
+
+        }
+}
+
