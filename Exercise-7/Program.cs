01a0ef777e70d45653da206cabf98dba22bfc624
Shah Newaz <shahnewaz385@gmail.com>
2020-01-06 20:19:52 +0600

    7

---
+++ Exercise-7/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercise_7
+{
+    class Program
+    {
+        public static void Main()
+        {
+
+            Console.Write("Enter a number: ");
+            var number = Convert.ToInt32(Console.ReadLine());
+
+            var factorial = 1;
+            for (var i = 1; i <= number; i++)
+                factorial *= i;
+
+            Console.WriteLine("{0}! = {1}", number, factorial);
+
+        }
+
+
+
+    }
+}
