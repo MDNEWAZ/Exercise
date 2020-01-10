98a6c0d8fbcdc961393ef64720dd2472273d5358
Shah Newaz <shahnewaz385@gmail.com>
2020-01-11 02:15:15 +0600

    Exercise-13--

---
+++ Exercise-13/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+/// <summary>
+/// Write a program and ask the user to continuously enter a number or type "Quit" to exit. The list of numbers may 
+/// include duplicates. Display the unique numbers that the user has entered.
+/// </summary>
+
+namespace Exercise_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int>();
+
+            while (true)
+            {
+               
+                 Console.WriteLine("Enter a number continiously Or type Quit exit then show all the unique number without dublicate");
+                 var input = Console.ReadLine();
+
+                if (input.ToLower() == "quit")
+                    break;
+                numbers.Add(Convert.ToInt32(input));
+
+            }
+            var uniques = new List<int>();
+            foreach (var input in numbers)
+            {
+                if (!uniques.Contains(input))
+                    uniques.Add(input);
+            }
+
+            Console.WriteLine("Unique numbers:");
+            foreach (var input in uniques)
+                Console.WriteLine(input);
+
+
+
+
+        }
+    }
+}
