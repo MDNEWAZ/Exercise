884a250f3f11468a322094889a3a40422643532f
Shah Newaz <shahnewaz385@gmail.com>
2020-01-12 11:37:48 +0600

    Exercise-15--

---
+++ Exercise-15/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercise_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            while (true)
+            {
+                
+                Console.WriteLine("Enter a few numbers separated by a hyphen");
+                 var numbers =  Console.ReadLine();
+                
+                Console.WriteLine(numbers.Split("-"));
+                Console.ReadKey();
+
+
+
+
+            }
+           
+
+        }
+    }
+}
